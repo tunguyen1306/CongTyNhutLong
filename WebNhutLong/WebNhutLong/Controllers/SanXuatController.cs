91f36e9fde0996ffd195dcdda67660b69a59afcb
nguyễn tấn tu <tunguyen.it.dev@gmail.com>
2016-02-22 01:18:08 +0700

    get

---
+++ WebNhutLong/WebNhutLong/Controllers/SanXuatController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebNhutLong.Models;
+
+namespace WebNhutLong.Controllers
+{
+    public class SanXuatController : Controller
+    {
+        private WebNhutLongEntities db = new WebNhutLongEntities();
+        // GET: SanXuat
+        public ActionResult Index(int id)
+        {
+            var qr = from data in db.tbl_OrderTem
+                where data.status == id
+                select data;
+            return View(qr.ToList());
+        }
+    }
+}
