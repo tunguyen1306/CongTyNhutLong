70b69b3deafc6858c12a68686ff622f65ea10e37
nguyễn tấn tu <tunguyen.it.dev@gmail.com>
2016-02-22 02:04:36 +0700

    edit

--- WebNhutLong/WebNhutLong/Controllers/SanXuatController.cs
+++ WebNhutLong/WebNhutLong/Controllers/SanXuatController.cs
@@ -13,9 +13,24 @@ namespace WebNhutLong.Controllers
         // GET: SanXuat
         public ActionResult Index()
         {
-            var qr = from data in db.tbl_OrderTem
+          
+            var qr = (from data in db.tbl_OrderTem
+                join cus in db.tbl_Customers on data.customer_id equals cus.IDCustomers
                 where data.status == 1
-                select data;
+                select new DonHangView
+                {
+                    id = data.id,
+                    customer_id = cus.IDCustomers,
+                    Customer = cus,
+                    code=data.code,
+                    date_begin = data.date_begin,
+                    date_end = data.date_end,
+                    status = data.status
+                        
+
+                    
+                });
+           
             return View(qr.ToList());
         }
     }
