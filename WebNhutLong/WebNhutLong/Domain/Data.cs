00a34dd134bdf85ca2df561618e528a522f2ba68
nguyễn tấn tu <tunguyen.it.dev@gmail.com>
2016-02-24 00:29:18 +0700

    no message

--- WebNhutLong/WebNhutLong/Domain/Data.cs
+++ WebNhutLong/WebNhutLong/Domain/Data.cs
@@ -16,7 +16,8 @@ namespace WebNhutLong.Domain
             //menu.Add(new Navbar { Id = 20, nameOption = "Sản Phẩm", controller = "Products", action = "Index", imageClass = "fa fa-bar-chart-o fa-fw", status = true, isParent = false, parentId = 0 });
            
             menu.Add(new Navbar { Id = 22, nameOption = "Lịch sản xuất", controller = "Sanxuat", action = "Index", imageClass = "fa fa-bar-chart-o fa-fw", status = true, isParent = false, parentId = 0 });
-            menu.Add(new Navbar { Id = 23, nameOption = "Báo cáo", controller = "DonHang", action = "BaoCao", imageClass = "fa fa-bar-chart-o fa-fw", status = true, isParent = false, parentId = 0 });
+            menu.Add(new Navbar { Id = 23, nameOption = "Báo cáo", controller = "BaoCao", action = "Index", imageClass = "fa fa-bar-chart-o fa-fw", status = true, isParent = true, parentId = 0 });
+            menu.Add(new Navbar { Id = 24, nameOption = "Báo cáo tổng quát cho khâu sản xuất", controller = "BaoCao", action = "Index", imageClass = "fa fa-bar-chart-o fa-fw", status = true, isParent = false, parentId = 23 });
            
 
             return menu.ToList();
