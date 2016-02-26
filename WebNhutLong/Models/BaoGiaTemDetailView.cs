25edb3242fc08bde0926263dbf01556dbb507da3
nguyen tan tu <tunguyen.it.dev@gmail.com>
2016-02-27 03:34:09 +0700

    no message

--- WebNhutLong/Models/BaoGiaTemDetailView.cs
+++ WebNhutLong/Models/BaoGiaTemDetailView.cs
@@ -27,6 +27,9 @@ namespace WebNhutLong.Models
 
         public Nullable<int> Design { get; set; }
         public String Design_Img { get; set; }
-        public Nullable<DateTime>  Design_Date { get; set; }
+        public Nullable<DateTime> Design_Date { get; set; }
+
+        public Nullable<int> Timer { get; set; }
+        public Nullable<System.DateTime> date_deliver { get; set; }
     }
 }
